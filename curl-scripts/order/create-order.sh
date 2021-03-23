09f514a73bdb5c045bc111f07a6f0bb889b02ab3
sven-gerlach <svengerlach@me.com>
2021-03-23 14:25:51 -0400

    Amend create order curl-script
    Removed owner and order-item from body
    
    Co-authored by: Connie Panzariello
    <connie.panzariello@gmail.com

--- curl-scripts/order/create-order.sh
+++ curl-scripts/order/create-order.sh
@@ -3,16 +3,14 @@
 API="http://localhost:4741"
 URL_PATH="/orders"
 
-curl "${API}${URL_PATH}/" \
+curl "${API}${URL_PATH}/${ID}" \
   --include \
   --request POST \
   --header "Authorization: Bearer ${TOKEN}" \
   --header "Content-Type: application/json" \
   --data '{
     "order": {
-      "status": "'"cart"'",
-      "owner": "'"${OWNER}"'",
-      "orderItems": "'"[]"'"
+      "status": "'"cart"'"
     }
   }'
 
