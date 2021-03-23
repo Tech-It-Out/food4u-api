6a79e326294f3b921761cebb07908ec498276a04
oscar-valen115 <oscar.valen115@gmail.com>
2021-03-23 18:16:49 -0400

    Modified user model, added curl script for updating the user data, and  created the update route route to update the user data.  Still pending  completion though.

---
+++ curl-scripts/auth/update-user.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+API="http://localhost:4741"
+URL_PATH="/update"
+
+curl "${API}${URL_PATH}/" \
+  --include \
+  --request PATCH \
+  --header "Authorization: Bearer ${TOKEN}" \
+  --header "Content-Type: application/json" \
+  --data '{
+    "user": {
+      "email": "'"${EMAIL}"'",
+      "firstName": "'"${FIRSTNAME}"'",
+      "surname": "'"${SURNAME}"'",
+      "street": "'"${STREET}"'",
+      "apartment": "'"${APARTMENT}"'",
+      "state": "'"${STATE}"'",
+      "country": "'"${COUNTRY}"'",
+    }
+  }'
+
+echo
